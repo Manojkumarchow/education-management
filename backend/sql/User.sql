d8d853ab2d29f4a053ac8e2d0ab9570d959b57fc
Manoj Chowdary <manojchow72@gmail.com>
2023-02-06 22:54:29 +0530

    User Module and Login

--- backend/sql/User.sql
+++ backend/sql/User.sql
@@ -2,16 +2,19 @@ create database education_management;
 
 use education_management;
 
-CREATE TABLE Users (
+CREATE TABLE users (
   id INT AUTO_INCREMENT PRIMARY KEY,
   username VARCHAR(100) NOT NULL,
   password VARCHAR(100) NOT NULL,
-  category VARCHAR(100) NOT NULL
+  category VARCHAR(100) NOT NULL,
+  access_level varchar(10) DEFAULT NULL
 );
 
-INSERT INTO Users (username, password, category) VALUES
-  ('user1', 'password1', 'category1'),
-  ('user2', 'password2', 'category2'),
-  ('user3', 'password3', 'category3'),
-  ('user4', 'password4', 'category4'),
-  ('user5', 'password5', 'category5');
+INSERT INTO users (username, password, category, access_level) VALUES
+  ('user1', 'password1', 'category1', '0'),
+  ('user2', 'password2', 'category2', '0'),
+  ('user3', 'password3', 'category3', '0'),
+  ('user4', 'password4', 'category4', '0'),
+  ('user5', 'password5', 'category5', '0');
+INSERT INTO users (username, password, category, access_level) VALUES
+  ('manoj', 'manoj', 'category1', '0');
--- backend/src/main/java/com/education/backend/models/UserDTO.java
+++ backend/src/main/java/com/education/backend/models/UserDTO.java
@@ -10,7 +10,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "Users")
+@Table(name = "users")
 public class UserDTO {
 
 	@Id
